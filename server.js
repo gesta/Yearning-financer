72bd7d7d3b9395906acdfb95385f160f87109591
Gerasim Stanchev <gero@tutuf.com>
2017-02-17 14:47:16 +0200

    Add method for loading stocks data for yesterday

--- server.js
+++ server.js
@@ -43,13 +43,57 @@ server.get('/weekly_json', (_, res) => {
   });
 });
 
-//http.get("http://ichart.finance.yahoo.com/table.csv?s=GOOG&ignore=.csv", (response) => {
-  // The callback provides the response readable stream.
-  // Then, we open our output text stream.
-  //var outStream = require('fs').createWriteStream("out.txt");
+var formatDate = (utcDate) => {
+  if (typeof utcDate === 'undefined') { utcDate = new Date(); }
+  return utcDate
+    .toISOString()
+    .replace(/T.*/, ' ')
+    .trim();
+};
 
-    // Pipe the input to the output, which writes the file.
-  //response.pipe(outStream);
-//});
+var getTodayStocks = function() {
+  let requestBuilder = 'https://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.historicaldata where symbol = "GSPC" and startDate = "' +
+        formatDate(new Date(new Date().setDate(new Date().getDate() - 1))) + '" and endDate = "' + formatDate(new Date()) +
+        '"&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys';
+
+  https.get(requestBuilder, (response) => {
+    response.on('data', (data) => {
+      setTimeout( () => {
+        let resp = JSON.parse(data)['query']['results']['quote'];
+        let result = {};
+        Object.keys(resp).forEach( (id) => {
+          switch (id) {
+          case 'Date':
+            result['day'] = resp[id];
+            break;
+          case 'Open':
+            result['open'] = resp[id];
+            break;
+          case 'Close':
+            result['close'] = resp[id];
+            break;
+          case 'High':
+            result['high'] = resp[id];
+            break;
+          case 'Low':
+            result['low'] = resp[id];
+            break;
+          case 'Volume':
+            result['volume'] = resp[id];
+            break;
+          case 'Adj_Close':
+            result['adj'] = resp[id];
+            break;
+          }
+        });
+        DailyStock.findOrCreate({where: result});
+      }, 1000);
+    });
+
+    response.on('error', (err) => {
+      console.log(err);
+    });
+  });
+};
 
 server.listen(process.env.PORT || 5000);
